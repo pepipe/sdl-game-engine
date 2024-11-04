6c5afc4d79ea369a33d5a309bbb6f62b6c8d39a3
pepipe <pepipee@gmail.com>
2024-11-04 23:03:45 +0000

    Prepared Audio for game-engine

--- src/game-engine/audio/Audio.cpp
+++ src/game-engine/audio/Audio.cpp
@@ -1,6 +1,43 @@
 ﻿#include "Audio.h"
 
-
-Audio::Audio()
+Audio::~Audio()
 {
+    CleanUp();
+}
+
+bool Audio::InitializeAudio(const SDL_AudioDeviceID deviceId, const SDL_AudioSpec* desiredSpec)
+{
+    if (Mix_OpenAudio(deviceId, desiredSpec) == false)
+    {
+        SDL_Log("Failed to open audio device: %s", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void Audio::LoadSound(const std::string& name, const std::string& path)
+{
+    Mix_Chunk* sound = Mix_LoadWAV(path.c_str());
+    if (!sound)
+    {
+        SDL_Log("Failed to load sound %s: %s", path.c_str(), SDL_GetError());
+        return;
+    }
+    _sounds[name] = sound;
+}
+
+void Audio::PlaySound(const std::string& name, const int loops)
+{
+    if (const auto it = _sounds.find(name); it != _sounds.end())
+    {
+        Mix_PlayChannel(-1, it->second, loops);
+    }
+}
+
+void Audio::CleanUp() {
+    for (const auto& sound : _sounds) {
+        Mix_FreeChunk(sound.second);
+    }
+    _sounds.clear();
+    Mix_CloseAudio();
 }
--- src/game-engine/audio/Audio.h
+++ src/game-engine/audio/Audio.h
@@ -1,16 +1,19 @@
 ﻿#pragma once
+
 #include <SDL3_mixer/SDL_mixer.h>
 #include <string>
+#include <unordered_map>
 
 class Audio
 {
 public:
-    Audio();
     ~Audio();
 
-    bool Init(int frequency = 44100, Uint16 format = SDL_AUDIO_S16, int channels = 2, int chunkSize = 4096);
+    static bool InitializeAudio(SDL_AudioDeviceID deviceId, const SDL_AudioSpec* desiredSpec);
     void LoadSound(const std::string& name, const std::string& path);
     void PlaySound(const std::string& name, int loops = 0);
     void CleanUp();
+
 private:
+    std::unordered_map<std::string, Mix_Chunk*> _sounds;
 };
